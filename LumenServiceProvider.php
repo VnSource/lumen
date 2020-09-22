9f31f2ae806040f4540621f67098ba315d8ca63b
Mr. VnS <lienhe.vns@gmail.com>
2020-09-22 21:06:56 +0700

    Create LumenServiceProvider.php

---
+++ LumenServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace VnSource\Lumen;
+
+use Illuminate\Support\ServiceProvider;
+
+class LumenServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
